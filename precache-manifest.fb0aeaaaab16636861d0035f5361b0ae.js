369bd540d44295553a82f5a07a195a2f5b92722a
Balpreet Singh <knowbalpreet@Balpreets-MacBook-Pro.local>
2018-12-30 16:54:56 +0530

    Updates

---
+++ precache-manifest.fb0aeaaaab16636861d0035f5361b0ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8acda8d826375bba5da5",
+    "url": "/knowBalpreet/quze-frontend/static/js/runtime~main.8acda8d8.js"
+  },
+  {
+    "revision": "abed25d79b2ef786cb15",
+    "url": "/knowBalpreet/quze-frontend/static/js/main.abed25d7.chunk.js"
+  },
+  {
+    "revision": "d09d48454ebccf5e81c2",
+    "url": "/knowBalpreet/quze-frontend/static/js/1.d09d4845.chunk.js"
+  },
+  {
+    "revision": "abed25d79b2ef786cb15",
+    "url": "/knowBalpreet/quze-frontend/static/css/main.addec964.chunk.css"
+  },
+  {
+    "revision": "d09d48454ebccf5e81c2",
+    "url": "/knowBalpreet/quze-frontend/static/css/1.ed59bfb0.chunk.css"
+  },
+  {
+    "revision": "4a9101c1af163fccd45ed125a925fd79",
+    "url": "/knowBalpreet/quze-frontend/index.html"
+  }
+];
